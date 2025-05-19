5b94762daa6c980176e8f16cc0426a2ce8c13827
Luis Rubi <luisorubi@hotmail.com>
2025-05-19 11:59:00 -0600

    first snake game commit

---
+++ snake.py
@@ -0,0 +1,29 @@
+import pygame
+import sys
+
+pygame.init()
+
+
+width, height = 600, 400
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Snake Game")
+clock = pygame.time.Clock()
+snake_speed = 6
+
+def game_loop():
+    running = True
+    while running:
+        screen.fill((50, 153, 213))
+    
+        for event in pygame.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
+        pygame.display.update()
+        clock.tick(snake_speed)
+    
+    pygame.quit()
+    sys.exit()
+
+game_loop()
+
