d078e97a95bd03c50cb51b1567c8bd61521e6364
Luis Rubi <luisorubi@hotmail.com>
2025-05-19 12:27:45 -0600

    adding colors that I'll be using for the project

--- snake.py
+++ snake.py
@@ -3,6 +3,13 @@ import sys
 
 pygame.init()
 
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
 
 width, height = 600, 400
 screen = pygame.display.set_mode((width, height))
